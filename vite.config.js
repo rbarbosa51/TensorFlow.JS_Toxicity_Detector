d10ace88aae714b9b79c41555fd9e2eadce1da7d
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-08-25 17:20:14 -0500

    Added vite-plugin for html minification

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import htmlMinifier from 'vite-plugin-html-minifier';
 
 export default defineConfig({
-  base: "/",
+  base: "/test",
   server: {
     port: 8001,
   },
+  plugins: [
+    htmlMinifier({
+      minify: true
+    })
+  ]
 });
